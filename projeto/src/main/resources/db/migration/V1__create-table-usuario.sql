4b8961ec1c0bc21c22be888c9ec243557adfb9f5
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2024-03-01 20:06:35 -0300

    Migration -> sql usuario e doação.

---
+++ projeto/src/main/resources/db/migration/V1__create-table-usuario.sql
@@ -0,0 +1,6 @@
+CREATE TABLE usuario (
+    id INT PRIMARY KEY,
+    endereco VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
---
+++ projeto/src/main/resources/db/migration/V2__create-table-doacao.sql
@@ -0,0 +1,6 @@
+    CREATE TABLE doacao (
+    id INT PRIMARY KEY,
+    quantidade INT NOT NULL,
+    validade DATE NOT NULL,
+
+    );
